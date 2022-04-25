4b0b1db41340b203c755226176329e46b2aba9bf
Burstniart <v4gr33n@hotmail.com>
2022-04-25 13:19:37 -0500

    Add object creation test

--- test/another/another.test.js
+++ test/another/another.test.js
@@ -2,8 +2,17 @@ const Another = require('./../../app/another/Another')
 
 describe("Test Suite 101 by Lex", () => {
     test("1) I'm empty inside", () => {
-        const res = new Another()
+        const res = new Another('Monika', 0, ['Tulpish', 'English', 'Spanish'], 'Female', 'Lex')
 
         expect(res.sayHi()).toBe('Hi');
     });
+    test('2) Create object', () => {
+        const Mon = new Another('Monika', 0, ['Tulpish', 'English', 'Spanish'], 'Female', 'Lex')
+
+        expect (Mon.name).toBe('Monika')
+        expect (Mon.age).toBe(0)
+        expect (Mon.language).toBe(['Tulpish', 'English', 'Spanish'])
+        expect (Mon.gender).toBe('Female')
+        expect (Mon.host).toBe('Lex')
+    })
 })
