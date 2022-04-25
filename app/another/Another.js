8ac4d25d59e4859d3c05505258b805aa1986ed18
Burstniart <v4gr33n@hotmail.com>
2022-04-25 13:03:57 -0500

    Add simple function, test passed

--- app/another/Another.js
+++ app/another/Another.js
@@ -1,2 +1,7 @@
+class Another {
+    sayHi = () => {
+        return 'Hi'        
+    }
+}
 
 module.exports = Another
--- test/another/another.test.js
+++ test/another/another.test.js
@@ -2,7 +2,8 @@ const Another = require('./../../app/another/Another')
 
 describe("Test Suite 101 by Lex", () => {
     test("1) I'm empty inside", () => {
-        const res = 1 + 2
-        expect(res).toBe(45);
+        const res = new Another()
+
+        expect(res.sayHi()).toBe('Hi');
     });
 })
